24fc235d0ba1ff02cb6467e710fedddf6ae03374
marinewater <HartzFEAR@gmail.com>
2016-04-09 21:17:50 +0200

    added: isUndefined options

--- test/validators/isUndefined.spec.js
+++ test/validators/isUndefined.spec.js
@@ -6,75 +6,264 @@ const isUndefined = require( '../../lib/validators/isUndefined' );
 
 describe( 'isUndefined', function() {
 
-    it( 'should return false for true', function() {
+    describe( 'default options', function() {
 
-        expect( isUndefined( true ) ).to.be.false;
+        it( 'should return false for true', function() {
+
+            expect( isUndefined( true ) ).to.be.false;
+
+        });
+
+        it( 'should return false for false', function() {
+
+            expect( isUndefined( false ) ).to.be.false;
+
+        });
+
+        it( 'should return false for null', function() {
+
+            expect( isUndefined( null ) ).to.be.false;
+
+        });
+
+        it( 'should return true for no argument', function() {
+
+            expect( isUndefined() ).to.be.true;
+
+        });
+
+        it( 'should return true for undefined', function() {
+
+            expect( isUndefined( undefined ) ).to.be.true;
+
+        });
+
+        it( 'should return false for "undefined"', function() {
+
+            expect( isUndefined( 'undefined' ) ).to.be.false;
+
+        });
+
+        it( 'should return false for "UNDEFINED"', function() {
+
+            expect( isUndefined( 'UNDEFINED' ) ).to.be.false;
+
+        });
+
+        it( 'should return false for 1', function() {
+
+            expect( isUndefined( 1 ) ).to.be.false;
+
+        });
+
+        it( 'should return false for 0', function() {
+
+            expect( isUndefined( 0 ) ).to.be.false;
+
+        });
+
+        it( 'should return false for -1', function() {
+
+            expect( isUndefined( -1 ) ).to.be.false;
+
+        });
+
+        it( 'should return false for an empty string', function() {
+
+            expect( isUndefined( '' ) ).to.be.false;
+
+        });
+
+        it( 'should return false for a string', function() {
+
+            expect( isUndefined( 'a' ) ).to.be.false;
+
+        });
+
+        it( 'should return false for "true"', function() {
+
+            expect( isUndefined( 'true' ) ).to.be.false;
+
+        });
+
+        it( 'should return false for "false"', function() {
+
+            expect( isUndefined( 'false' ) ).to.be.false;
+
+        });
 
     });
 
-    it( 'should return false for false', function() {
+    describe( 'parse string', function() {
 
-        expect( isUndefined( false ) ).to.be.false;
+        const options = {
+            parseString: true
+        };
+
+        it( 'should return false for true', function() {
+
+            expect( isUndefined( true, options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for false', function() {
+
+            expect( isUndefined( false, options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for null', function() {
+
+            expect( isUndefined( null, options ) ).to.be.false;
+
+        });
+
+        it( 'should return true for undefined', function() {
+
+            expect( isUndefined( undefined, options ) ).to.be.true;
+
+        });
+
+        it( 'should return true for "undefined"', function() {
+
+            expect( isUndefined( 'undefined', options ) ).to.be.true;
+
+        });
+
+        it( 'should return false for "UNDEFINED"', function() {
+
+            expect( isUndefined( 'UNDEFINED', options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for 1', function() {
+
+            expect( isUndefined( 1, options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for 0', function() {
+
+            expect( isUndefined( 0, options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for -1', function() {
+
+            expect( isUndefined( -1, options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for an empty string', function() {
+
+            expect( isUndefined( '', options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for a string', function() {
+
+            expect( isUndefined( 'a', options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for "true"', function() {
+
+            expect( isUndefined( 'true', options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for "false"', function() {
+
+            expect( isUndefined( 'false', options ) ).to.be.false;
+
+        });
 
     });
 
-    it( 'should return false for null', function() {
+    describe( 'case insensitive', function() {
 
-        expect( isUndefined( null ) ).to.be.false;
+        const options = {
+            parseString: true,
+            caseSensitive: false
+        };
 
-    });
+        it( 'should return false for true', function() {
 
-    it( 'should return true for no argument', function() {
+            expect( isUndefined( true, options ) ).to.be.false;
 
-        expect( isUndefined() ).to.be.true;
+        });
 
-    });
+        it( 'should return false for false', function() {
 
-    it( 'should return true for undefined', function() {
+            expect( isUndefined( false, options ) ).to.be.false;
 
-        expect( isUndefined( undefined ) ).to.be.true;
+        });
 
-    });
+        it( 'should return false for null', function() {
 
-    it( 'should return false for 1', function() {
+            expect( isUndefined( null, options ) ).to.be.false;
 
-        expect( isUndefined( 1 ) ).to.be.false;
+        });
 
-    });
+        it( 'should return true for undefined', function() {
 
-    it( 'should return false for 0', function() {
+            expect( isUndefined( undefined, options ) ).to.be.true;
 
-        expect( isUndefined( 0 ) ).to.be.false;
+        });
 
-    });
+        it( 'should return true for "undefined"', function() {
 
-    it( 'should return false for -1', function() {
+            expect( isUndefined( 'undefined', options ) ).to.be.true;
 
-        expect( isUndefined( -1 ) ).to.be.false;
+        });
 
-    });
+        it( 'should return true for "UNDEFINED"', function() {
 
-    it( 'should return false for an empty string', function() {
+            expect( isUndefined( 'UNDEFINED', options ) ).to.be.true;
 
-        expect( isUndefined( '' ) ).to.be.false;
+        });
 
-    });
+        it( 'should return false for 1', function() {
 
-    it( 'should return false for a string', function() {
+            expect( isUndefined( 1, options ) ).to.be.false;
 
-        expect( isUndefined( 'a' ) ).to.be.false;
+        });
 
-    });
+        it( 'should return false for 0', function() {
 
-    it( 'should return false for "true"', function() {
+            expect( isUndefined( 0, options ) ).to.be.false;
 
-        expect( isUndefined( 'true' ) ).to.be.false;
+        });
 
-    });
+        it( 'should return false for -1', function() {
 
-    it( 'should return false for "false"', function() {
+            expect( isUndefined( -1, options ) ).to.be.false;
 
-        expect( isUndefined( 'false' ) ).to.be.false;
+        });
+
+        it( 'should return false for an empty string', function() {
+
+            expect( isUndefined( '', options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for a string', function() {
+
+            expect( isUndefined( 'a', options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for "true"', function() {
+
+            expect( isUndefined( 'true', options ) ).to.be.false;
+
+        });
+
+        it( 'should return false for "false"', function() {
+
+            expect( isUndefined( 'false', options ) ).to.be.false;
+
+        });
 
     });
 
