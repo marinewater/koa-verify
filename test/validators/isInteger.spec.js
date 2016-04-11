500f054d67a8de7d15388a128ad7dfd46117a6a0
marinewater <HartzFEAR@gmail.com>
2016-04-11 22:43:25 +0200

    Revert "added: parseString option to isInteger"
    This reverts commit 47c8b29fd6775d651c01cd705c23ae3687a856fa.

--- test/validators/isInteger.spec.js
+++ test/validators/isInteger.spec.js
@@ -5,189 +5,81 @@ const isInteger = require( '../../lib/validators/isInteger' );
 
 describe( 'isInteger', function() {
 
-    describe( 'default options', function() {
+    it( 'should return true for 1', function() {
 
-        it( 'should return true for 1', function() {
-
-            expect( isInteger( 1 ) ).to.be.true;
-
-        });
-
-        it( 'should return true for 0', function() {
-
-            expect( isInteger( 0 ) ).to.be.true;
-
-        });
-
-        it( 'should return true for -1', function() {
-
-            expect( isInteger( -1 ) ).to.be.true;
-
-        });
-
-        it( 'should return false for 0.1', function() {
-
-            expect( isInteger( 0.1 ) ).to.be.false;
-
-        });
-
-        it( 'should return false for -0.1', function() {
-
-            expect( isInteger( -0.1 ) ).to.be.false;
-
-        });
-
-        it( 'should return false for "0"', function() {
-
-            expect( isInteger( '0' ) ).to.be.false;
-
-        });
-
-        it( 'should return false for "1"', function() {
-
-            expect( isInteger( '1' ) ).to.be.false;
-
-        });
-
-        it( 'should return false for "-1"', function() {
-
-            expect( isInteger( '-1' ) ).to.be.false;
-
-        });
-
-        it( 'should return false for "0.1"', function() {
-
-            expect( isInteger( '0.1' ) ).to.be.false;
-
-        });
-
-        it( 'should return false for "-0.1"', function() {
-
-            expect( isInteger( '-0.1' ) ).to.be.false;
-
-        });
-
-        it( 'should return false for true', function() {
-
-            expect( isInteger( true ) ).to.be.false;
-
-        });
-
-        it( 'should return false for false', function() {
-
-            expect( isInteger( false ) ).to.be.false;
-
-        });
-
-        it( 'should return false for null', function() {
-
-            expect( isInteger( null ) ).to.be.false;
-
-        });
-
-        it( 'should return false for undefined', function() {
-
-            expect( isInteger( undefined ) ).to.be.false;
-
-        });
-
-        it( 'should return false for no argument', function() {
-
-            expect( isInteger() ).to.be.false;
-
-        });
+        expect( isInteger( 1 ) ).to.be.true;
 
     });
 
-    describe( 'parse string', function() {
+    it( 'should return true for 0', function() {
 
-        const options = {
-            parseString: true
-        };
+        expect( isInteger( 0 ) ).to.be.true;
 
-        it( 'should return true for 1', function() {
+    });
 
-            expect( isInteger( 1, options ) ).to.be.true;
+    it( 'should return true for -1', function() {
 
-        });
+        expect( isInteger( -1 ) ).to.be.true;
 
-        it( 'should return true for 0', function() {
+    });
 
-            expect( isInteger( 0, options ) ).to.be.true;
+    it( 'should return false for 0.1', function() {
 
-        });
+        expect( isInteger( 0.1 ) ).to.be.false;
 
-        it( 'should return true for -1', function() {
+    });
 
-            expect( isInteger( -1, options ) ).to.be.true;
+    it( 'should return false for -0.1', function() {
 
-        });
+        expect( isInteger( -0.1 ) ).to.be.false;
 
-        it( 'should return false for 0.1', function() {
+    });
 
-            expect( isInteger( 0.1, options ) ).to.be.false;
+    it( 'should return false for "0"', function() {
 
-        });
+        expect( isInteger( '0' ) ).to.be.false;
 
-        it( 'should return false for -0.1', function() {
+    });
 
-            expect( isInteger( -0.1, options ) ).to.be.false;
+    it( 'should return false for "1"', function() {
 
-        });
+        expect( isInteger( '1' ) ).to.be.false;
 
-        it( 'should return true for "0"', function() {
+    });
 
-            expect( isInteger( '0', options ) ).to.be.true;
+    it( 'should return false for "-1"', function() {
 
-        });
+        expect( isInteger( '-1' ) ).to.be.false;
 
-        it( 'should return true for "1"', function() {
+    });
 
-            expect( isInteger( '1', options ) ).to.be.true;
+    it( 'should return false for true', function() {
 
-        });
+        expect( isInteger( true ) ).to.be.false;
 
-        it( 'should return true for "-1"', function() {
+    });
 
-            expect( isInteger( '-1', options ) ).to.be.true;
+    it( 'should return false for false', function() {
 
-        });
+        expect( isInteger( false ) ).to.be.false;
 
-        it( 'should return false for "0.1"', function() {
+    });
 
-            expect( isInteger( '0.1', options ) ).to.be.false;
+    it( 'should return false for null', function() {
 
-        });
+        expect( isInteger( null ) ).to.be.false;
 
-        it( 'should return false for "-0.1"', function() {
+    });
 
-            expect( isInteger( '-0.1', options ) ).to.be.false;
+    it( 'should return false for undefined', function() {
 
-        });
+        expect( isInteger( undefined ) ).to.be.false;
 
-        it( 'should return false for true', function() {
+    });
 
-            expect( isInteger( true, options ) ).to.be.false;
+    it( 'should return false for no argument', function() {
 
-        });
-
-        it( 'should return false for false', function() {
-
-            expect( isInteger( false, options ) ).to.be.false;
-
-        });
-
-        it( 'should return false for null', function() {
-
-            expect( isInteger( null, options ) ).to.be.false;
-
-        });
-
-        it( 'should return false for undefined', function() {
-
-            expect( isInteger( undefined, options ) ).to.be.false;
-
-        });
+        expect( isInteger() ).to.be.false;
 
     });
 
