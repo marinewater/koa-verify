1630f304a2ba096b4563a4c4ba53a1f3a031c631
marinewater <HartzFEAR@gmail.com>
2016-04-13 03:40:58 +0200

    added: tests for isInteger

--- test/middleware/isInteger.spec.js
+++ test/middleware/isInteger.spec.js
@@ -4,10 +4,11 @@ const supertest = require( 'supertest' );
 
 const koa = require( 'koa' );
 const koa_body = require( 'koa-body' );
+const koa_router = require( 'koa-router' );
 
 const middleware = require( '../../lib/middleware' )();
 
-describe( 'middleware', function() {
+describe( 'middleware isInteger', function() {
 
     let app;
 
@@ -21,203 +22,373 @@ describe( 'middleware', function() {
 
     describe( 'isInteger', function() {
 
-        it( 'should validate an integer', function( done ) {
+        describe( 'checkBody', function() {
 
-            app.use( function *() {
+            it( 'should validate an integer', function( done ) {
 
-                this.checkBody( 'integer' ).isInteger();
+                app.use( function *() {
 
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
+                    this.checkBody( 'integer' ).isInteger();
 
-            });
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
 
-            let request = supertest( app.listen() );
+                });
 
-            request.post( '/' )
-                .send({
-                    integer: 1
-                })
-                .expect( 200, done );
+                let request = supertest( app.listen() );
 
-        } );
+                request.post( '/' )
+                    .send({
+                        integer: 1
+                    })
+                    .expect( 200, done );
 
-        it( 'should validate zero', function( done ) {
+            } );
 
-            app.use( function *() {
+            it( 'should validate zero', function( done ) {
 
-                this.checkBody( 'integer' ).isInteger();
+                app.use( function *() {
 
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
+                    this.checkBody( 'integer' ).isInteger();
 
-            });
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
 
-            let request = supertest( app.listen() );
+                });
 
-            request.post( '/' )
-                .send({
-                    integer: 0
-                })
-                .expect( 200, done );
+                let request = supertest( app.listen() );
 
-        } );
+                request.post( '/' )
+                    .send({
+                        integer: 0
+                    })
+                    .expect( 200, done );
 
-        it( 'should validate a negative value', function( done ) {
+            } );
 
-            app.use( function *() {
+            it( 'should validate a negative value', function( done ) {
 
-                this.checkBody( 'integer' ).isInteger();
+                app.use( function *() {
 
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
+                    this.checkBody( 'integer' ).isInteger();
 
-            });
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
 
-            let request = supertest( app.listen() );
+                });
 
-            request.post( '/' )
-                .send({
-                    integer: -1
-                })
-                .expect( 200, done );
+                let request = supertest( app.listen() );
 
-        } );
+                request.post( '/' )
+                    .send({
+                        integer: -1
+                    })
+                    .expect( 200, done );
 
-        it( 'should not validate a float', function( done ) {
+            } );
 
-            app.use( function *() {
+            it( 'should not validate a float', function( done ) {
 
-                this.checkBody( 'integer' ).isInteger();
+                app.use( function *() {
 
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
+                    this.checkBody( 'integer' ).isInteger();
 
-            });
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
 
-            let request = supertest( app.listen() );
+                });
 
-            request.post( '/' )
-                .send({
-                    integer: 0.1
-                })
-                .expect( 400, done );
+                let request = supertest( app.listen() );
 
-        } );
+                request.post( '/' )
+                    .send({
+                        integer: 0.1
+                    })
+                    .expect( 400, done );
 
-        it( 'should not validate a string', function( done ) {
+            } );
 
-            app.use( function *() {
+            it( 'should not validate a string', function( done ) {
 
-                this.checkBody( 'integer' ).isInteger();
+                app.use( function *() {
 
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
+                    this.checkBody( 'integer' ).isInteger();
 
-            });
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
 
-            let request = supertest( app.listen() );
+                });
 
-            request.post( '/' )
-                .send({
-                    integer: '1'
-                })
-                .expect( 400, done );
+                let request = supertest( app.listen() );
 
-        } );
-        
-        it( 'should not validate null', function( done ) {
+                request.post( '/' )
+                    .send({
+                        integer: '1'
+                    })
+                    .expect( 400, done );
 
-            app.use( function *() {
+            } );
 
-                this.checkBody( 'integer' ).isInteger();
+            it( 'should not validate null', function( done ) {
 
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
+                app.use( function *() {
 
-            });
+                    this.checkBody( 'integer' ).isInteger();
 
-            let request = supertest( app.listen() );
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
 
-            request.post( '/' )
-                .send({
-                    integer: null
-                })
-                .expect( 400, done );
+                });
 
-        } );
+                let request = supertest( app.listen() );
 
-        it( 'should not validate undefined', function( done ) {
+                request.post( '/' )
+                    .send({
+                        integer: null
+                    })
+                    .expect( 400, done );
 
-            app.use( function *() {
+            } );
 
-                this.checkBody( 'integer' ).isInteger();
+            it( 'should not validate undefined', function( done ) {
 
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
+                app.use( function *() {
 
-            });
+                    this.checkBody( 'integer' ).isInteger();
 
-            let request = supertest( app.listen() );
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
 
-            request.post( '/' )
-                .send({
-                    integer: undefined
-                })
-                .expect( 400, done );
+                });
 
-        } );
+                let request = supertest( app.listen() );
 
-        it( 'should not validate an empty body', function( done ) {
+                request.post( '/' )
+                    .send({
+                        integer: undefined
+                    })
+                    .expect( 400, done );
 
-            app.use( function *() {
+            } );
 
-                this.checkBody( 'integer' ).isInteger();
+            it( 'should not validate an empty body', function( done ) {
 
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
+                app.use( function *() {
 
-            });
+                    this.checkBody( 'integer' ).isInteger();
 
-            let request = supertest( app.listen() );
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
 
-            request.post( '/' )
-                .send({})
-                .expect( 400, done );
+                });
 
-        } );
+                let request = supertest( app.listen() );
+
+                request.post( '/' )
+                    .send({})
+                    .expect( 400, done );
+
+            } );
+
+        });
+
+        describe( 'checkParams', function() {
+
+            it( 'should validate an integer', function( done ) {
+
+                let router = koa_router();
+
+                router.get( '/:integer', function *() {
+
+                    this.checkParams( 'integer' ).isInteger();
+
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
+
+                });
+
+                app.use( router.routes() );
+
+                let request = supertest( app.listen() );
+
+                request.get( '/1' )
+                    .expect( 200 )
+                    .end( done );
+
+            } );
+
+            it( 'should validate zero', function( done ) {
+
+                let router = koa_router();
+
+                router.get( '/:integer', function *() {
+
+                    this.checkParams( 'integer' ).isInteger();
+
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
+
+                });
+
+                app.use( router.routes() );
+
+                let request = supertest( app.listen() );
+
+                request.get( '/0' )
+                    .expect( 200 )
+                    .end( done );
+
+            } );
+
+            it( 'should validate a negative value', function( done ) {
+
+                let router = koa_router();
+
+                router.get( '/:integer', function *() {
+
+                    this.checkParams( 'integer' ).isInteger();
+
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
+
+                });
+
+                app.use( router.routes() );
+
+                let request = supertest( app.listen() );
+
+                request.get( '/-1' )
+                    .expect( 200 )
+                    .end( done );
+
+            } );
+
+            it( 'should not validate a float', function( done ) {
+
+                let router = koa_router();
+
+                router.get( '/:integer', function *() {
+
+                    this.checkParams( 'integer' ).isInteger();
+
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
+
+                });
+
+                app.use( router.routes() );
+
+                let request = supertest( app.listen() );
+
+                request.get( '/0.1' )
+                    .expect( 400 )
+                    .end( done );
+
+            } );
+
+            it( 'should not validate a string', function( done ) {
+
+                let router = koa_router();
+
+                router.get( '/:integer', function *() {
+
+                    this.checkParams( 'integer' ).isInteger();
+
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
+
+                });
+
+                app.use( router.routes() );
+
+                let request = supertest( app.listen() );
+
+                request.get( '/a' )
+                    .expect( 400 )
+                    .end( done );
+
+            } );
+
+            it( 'should not validate null', function( done ) {
+
+                let router = koa_router();
+
+                router.get( '/:integer', function *() {
+
+                    this.checkParams( 'integer' ).isInteger();
+
+                    if ( this.errors ) {
+                        this.status = 400;
+                    }
+                    else {
+                        this.status = 200;
+                    }
+
+                });
+
+                app.use( router.routes() );
+
+                let request = supertest( app.listen() );
+
+                request.get( '/null' )
+                    .expect( 400 )
+                    .end( done );
+
+            } );
+
+        });
 
     });
 
@@ -245,179 +416,6 @@ describe( 'middleware', function() {
                     integer: 1
                 })
                 .expect( 200, done );
-
-        } );
-
-        it( 'should validate zero', function( done ) {
-
-            app.use( function *() {
-
-                this.checkBody( 'integer' ).isInt();
-
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
-
-            });
-
-            let request = supertest( app.listen() );
-
-            request.post( '/' )
-                .send({
-                    integer: 0
-                })
-                .expect( 200, done );
-
-        } );
-
-        it( 'should validate a negative value', function( done ) {
-
-            app.use( function *() {
-
-                this.checkBody( 'integer' ).isInt();
-
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
-
-            });
-
-            let request = supertest( app.listen() );
-
-            request.post( '/' )
-                .send({
-                    integer: -1
-                })
-                .expect( 200, done );
-
-        } );
-
-        it( 'should not validate a float', function( done ) {
-
-            app.use( function *() {
-
-                this.checkBody( 'integer' ).isInt();
-
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
-
-            });
-
-            let request = supertest( app.listen() );
-
-            request.post( '/' )
-                .send({
-                    integer: 0.1
-                })
-                .expect( 400, done );
-
-        } );
-
-        it( 'should not validate a string', function( done ) {
-
-            app.use( function *() {
-
-                this.checkBody( 'integer' ).isInt();
-
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
-
-            });
-
-            let request = supertest( app.listen() );
-
-            request.post( '/' )
-                .send({
-                    integer: '1'
-                })
-                .expect( 400, done );
-
-        } );
-
-        it( 'should not validate null', function( done ) {
-
-            app.use( function *() {
-
-                this.checkBody( 'integer' ).isInt();
-
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
-
-            });
-
-            let request = supertest( app.listen() );
-
-            request.post( '/' )
-                .send({
-                    integer: null
-                })
-                .expect( 400, done );
-
-        } );
-
-        it( 'should not validate undefined', function( done ) {
-
-            app.use( function *() {
-
-                this.checkBody( 'integer' ).isInt();
-
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
-
-            });
-
-            let request = supertest( app.listen() );
-
-            request.post( '/' )
-                .send({
-                    integer: undefined
-                })
-                .expect( 400, done );
-
-        } );
-
-        it( 'should not validate an empty body', function( done ) {
-
-            app.use( function *() {
-
-                this.checkBody( 'integer' ).isInt();
-
-                if ( this.errors ) {
-                    this.status = 400;
-                }
-                else {
-                    this.status = 200;
-                }
-
-            });
-
-            let request = supertest( app.listen() );
-
-            request.post( '/' )
-                .send({})
-                .expect( 400, done );
 
         } );
 
